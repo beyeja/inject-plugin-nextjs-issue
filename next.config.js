de5b37f880cc4fe66362490e8df022cc8320707f
beyeja <beyeja@gmx.net>
2021-07-27 11:00:54 +0200

    fix to actually inject code that works properly

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const InjectPlugin = require('webpack-inject-plugin').default;
+const { ENTRY_ORDER } = require('webpack-inject-plugin');
 
 module.exports = {
 	reactStrictMode: true,
@@ -6,8 +7,9 @@ module.exports = {
 	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
 		config.plugins.push(
             new InjectPlugin(() => {
-				return "console.log('Hello World');"
+				return "console.log('hello world')";
 			}, {
+				entryOrder: ENTRY_ORDER.First,
 				// this is a really ugly fix to be able to use InjectPlugin with nextjs 11 and webpack 5
 				// entryName: (key) =>
 				// 	key !== 'chunkLoading' &&
@@ -20,6 +22,11 @@ module.exports = {
 				// 	key !== 'publicPath',
 			}),
         );
+
+		config.experiments = {
+			...config.experiments,
+			layers: true,
+		}
 		
 		return config;
 	}
